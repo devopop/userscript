70630be975baea206b2cf213e842fce784497fed
Ivan I <ii@mymailbox.pp.ua>
2014-08-08 01:13:48 +0300

    list inegrity

--- user.py
+++ user.py
@@ -8,16 +8,35 @@ from subprocess import call
 #pprint.pprint(globals())
 #pprint.pprint(locals())
 
-mosurl = 'https://docs.google.com/spreadsheets/d/1Qde18KtJX8rrCtkrtfar8JlG9k8iizJmESvhzPgt_-Y/export?gid=0&format=csv'
+HOMEDIR = "/home/"
 
-csvfile = urllib2.urlopen(mosurl)
+def createuser( name ):
+    print len(name)
+    #call(["useradd", "-m", name])
+
+def addkey( name, key ):
+    print len(name), len(key)
+
+#mosurl = 'https://docs.google.com/spreadsheets/d/1Qde18KtJX8rrCtkrtfar8JlG9k8iizJmESvhzPgt_-Y/export?gid=0&format=csv'
+
+#csvfile = urllib2.urlopen(mosurl)
+
+if 'csvfile' in locals():
+    file = open("mosurl.txt", "w")
+    file.write(csvfile.read())
+    file.close()
+
+csvfile = open("mosurl.txt", "r")
 
 # Splitting lines by delimiter
 while True:
         buf = csvfile.readline()
         if not buf: break
         namePair = buf.split(',')
-        print namePair[0]
+        if len(namePair) == 2 and len(namePair[0]) > 0 and len(namePair[1]) > 0:
+            #createuser(namePair[0])
+            addkey(namePair[0],namePair[1])
+
         #pprint.pprint(namePair);
 
-call(["useradd", "-m", namePair[0]])
+#call(["useradd", "-m", namePair[0]])
