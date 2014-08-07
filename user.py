ed7e844441791be666c30f624286092c70446baa
Ivan I <ii@mymailbox.pp.ua>
2014-08-07 15:35:34 +0300

    New way

--- user.py
+++ user.py
@@ -7,7 +7,7 @@ import urllib2
 mosurl = 'https://docs.google.com/spreadsheets/d/1Qde18KtJX8rrCtkrtfar8JlG9k8iizJmESvhzPgt_-Y/export?gid=0&format=csv'
 csvfile = urllib2.urlopen(mosurl)
 
-
+# Splitting lines by delimiter
 while True:
         buf = csvfile.readline()
         if not buf: break
