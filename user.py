dbfd68fd207f698cbfbb6f3552507d3947f0514c
Ivan I <ii@mymailbox.pp.ua>
2014-08-07 17:47:36 +0300

    getting user and his key seperately

--- user.py
+++ user.py
@@ -2,7 +2,10 @@
 
 import os
 import urllib2
+#import pprint
 
+#pprint.pprint(globals())
+#pprint.pprint(locals())
 
 mosurl = 'https://docs.google.com/spreadsheets/d/1Qde18KtJX8rrCtkrtfar8JlG9k8iizJmESvhzPgt_-Y/export?gid=0&format=csv'
 csvfile = urllib2.urlopen(mosurl)
@@ -11,5 +14,7 @@ csvfile = urllib2.urlopen(mosurl)
 while True:
         buf = csvfile.readline()
         if not buf: break
-        print buf.split(',')
+        namePair = buf.split(',')
+        print namePair[0]
+        #pprint.pprint(namePair);
 
