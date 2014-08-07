dd6368be8c1334bf1c1a6b6d989554e260f2333e
Ivan I <ii@mymailbox.pp.ua>
2014-08-07 15:22:22 +0300

    Deleted worthless comments

--- user.py
+++ user.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python
 
 import os
-#import csv
 import urllib2
-import StringIO
 
-#os.stat('./.pip')
 
 mosurl = 'https://docs.google.com/spreadsheets/d/1Qde18KtJX8rrCtkrtfar8JlG9k8iizJmESvhzPgt_-Y/export?gid=0&format=csv'
 csvfile = urllib2.urlopen(mosurl)
 
-#print csvfile.splitlines();
 
 while True:
-        buf = csvfile.readline();
+        buf = csvfile.readline()
         if not buf: break
-        #reader = csv.reader(buf,delimiter=',',quoting=csv.QUOTE_NONE)
         print buf.split(',')
 
